38e7f34543c86a793c08a8a950ee7fc7a8c40d16
Felipe Santos <felipefss@gmail.com>
2021-05-17 18:44:43 +0100

    Start adding logic to slushbucket.

--- src/components/Slushbucket/index.tsx
+++ src/components/Slushbucket/index.tsx
@@ -1,17 +1,49 @@
+import { useState } from "react";
 import styles from "./styles.module.scss";
 
-export default function Slushbucket({ circuitChoice = false }) {
+type DbItem = {
+  _id: number;
+  name: string;
+};
+
+type SlushbucketProps = {
+  availableItems: DbItem[];
+  circuitChoice?: boolean;
+  selected: DbItem[];
+  handleSelect: (item: DbItem) => void;
+};
+
+export default function Slushbucket({
+  availableItems,
+  circuitChoice = false,
+  selected,
+  handleSelect,
+}: SlushbucketProps) {
+  const [available, setAvailable] = useState(availableItems);
+  const [clickedAvailable, setClickedAvailable] = useState({} as DbItem);
+  // const [selected, setSelected] = useState([]);
+
   return (
     <>
       <section className={styles.players}>
         <div className={styles.choiceColumn}>
           <span>Disponíveis</span>
-          <select className={styles.choiceList} multiple></select>
+          <select className={styles.choiceList} multiple>
+            {available.map((item) => (
+              <option
+                key={item._id}
+                value={item._id}
+                onSelect={() => setClickedAvailable(item)}
+              >
+                {item.name}
+              </option>
+            ))}
+          </select>
         </div>
 
         <div className={styles.actionButtons}>
           <div className={styles.selectBtn} />
-          <button type="button">
+          <button type="button" onClick={}>
             {circuitChoice ? "Nova pista" : "Novo jogador"}
           </button>
           <div className={`${styles.selectBtn} ${styles.inverse}`} />
@@ -19,7 +51,13 @@ export default function Slushbucket({ circuitChoice = false }) {
 
         <div className={styles.choiceColumn}>
           <span>Selecionados</span>
-          <select className={styles.choiceList} multiple></select>
+          <select className={styles.choiceList} multiple>
+            {selected.map((item) => (
+              <option key={item._id} value={item._id}>
+                {item.name}
+              </option>
+            ))}
+          </select>
         </div>
       </section>
     </>
--- src/pages/new-championship/index.tsx
+++ src/pages/new-championship/index.tsx
@@ -1,13 +1,62 @@
+import { GetServerSideProps } from "next";
+import { useState } from "react";
+
+// DB
+import Players from "../../db/Players";
+
+// Components
 import Slushbucket from "../../components/Slushbucket";
+
+// Styles
 import styles from "./styles.module.scss";
 
-export default function NewChampionship() {
+type DbItem = {
+  _id: number;
+  name: string;
+};
+
+type NewChampionshipProps = {
+  players: DbItem[];
+  circuits: DbItem[];
+};
+
+export default function NewChampionship({ players }: NewChampionshipProps) {
+  // const [availablePlayers, setAvailablePlayers] = useState([]);
+  const [selectedPlayers, setSelectedPlayers] = useState([] as DbItem[]);
+  // const [availableCircuits, setAvailableCircuits] = useState([]);
+  const [selectedCircuits, setSelectedCircuits] = useState([] as DbItem[]);
+
+  const handleSelectPlayer = (item: DbItem) => {
+    setSelectedPlayers([...selectedPlayers, item]);
+  };
+
   return (
     <div className={styles.content}>
       <h2>Escolha os participantes</h2>
-      <Slushbucket />
+      <Slushbucket
+        availableItems={players}
+        selected={selectedPlayers}
+        handleSelect={handleSelectPlayer}
+      />
       <h2>Escolha as pistas</h2>
-      <Slushbucket circuitChoice />
+      {/* <Slushbucket circuitChoice /> */}
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const players = await (await Players()).getAllPlayers();
+
+  const parsedPlayers = players.map((p) => {
+    return {
+      _id: p._id,
+      name: p.name,
+    };
+  });
+
+  return {
+    props: {
+      players: parsedPlayers,
+    },
+  };
+};
